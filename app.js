c1573461a75bc773b957b6726c1b3a805f6360ce
edgargmaya <32275463+edgargmaya@users.noreply.github.com>
2025-07-25 14:15:46 -0600

    Update app.js

--- app.js
+++ app.js
@@ -36,3 +36,20 @@ const PORT = 3000;
 app.listen(PORT, () => {
     console.log(`Servidor escuchando en http://localhost:${PORT}`);
 });
+----
+import jenkins.model.*
+import hudson.security.*
+
+def instance = Jenkins.getInstance()
+
+println "--> Creating admin user"
+
+def hudsonRealm = new HudsonPrivateSecurityRealm(false)
+hudsonRealm.createAccount("admin", "admin123")
+instance.setSecurityRealm(hudsonRealm)
+
+def strategy = new FullControlOnceLoggedInAuthorizationStrategy()
+strategy.setAllowAnonymousRead(false)
+instance.setAuthorizationStrategy(strategy)
+
+instance.save()
